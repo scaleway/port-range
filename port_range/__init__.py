8c0f4396740911b345f9c79ff5f3e114ee1b9d3c
Kevin Deldycke <kdeldycke@ocs.online.net>
2015-03-04 11:10:30 +0100

    Bump copyright year.

--- port_range/__init__.py
+++ port_range/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2014 Online SAS and Contributors. All Rights Reserved.
-#                    Kevin Deldycke <kdeldycke@ocs.online.net>
-#                    Gilles Dartiguelongue <gdartiguelongue@ocs.online.net>
+# Copyright (c) 2014-2015 Online SAS and Contributors. All Rights Reserved.
+#                         Kevin Deldycke <kdeldycke@ocs.online.net>
+#                         Gilles Dartiguelongue <gdartiguelongue@ocs.online.net>
 #
 # Licensed under the BSD 2-Clause License (the "License"); you may not use this
 # file except in compliance with the License. You may obtain a copy of the
